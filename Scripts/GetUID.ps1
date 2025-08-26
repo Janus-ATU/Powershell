909dd1a01ea6fc25bf263730efeb5f122554c75a
John ORaw <janus@kmn.ie>
2025-08-26 10:07:18 +0100

    Create GetUID.ps1

---
+++ Scripts/GetUID.ps1
@@ -0,0 +1,22 @@
+﻿#
+.SYNOPSIS
+  <Get Linux info from AD>
+.DESCRIPTION
+  <Simple script to list existing AD users and their UID, GID, home directory>
+.OUTPUTS
+  <List of users sAMAccountName, UID , GID, home directory>.log>
+.NOTES
+  Version:        1.0
+  Author:         <JOR>
+  Creation Date:  <20250825>
+  Purpose/Change: Initial script  
+#>
+
+
+# Enable-PSRemoting -Force
+# Enter account details for an elevated user
+$Cred = Get-Credential
+# Login to dc1
+Enter-PSSession -ComputerName dc1 -Credential $Cred
+
+Get-ADUser -Filter * -Properties * | select SamAccountName,uidNumber, gidnumber, unixHomeDirectory
